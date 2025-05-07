32bf854886e484942b8ac5d0c547e273939207e9
Ivan Timoshenko <ivantm.dev@gmail.com>
2025-05-07 09:29:17 +0300

    .

--- ts-doc/src/app/app.component.ts
+++ ts-doc/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BaseComponent} from './basics/base/base.component';
+import {EverydayTypesComponent} from './basics/everyday-types/everyday-types.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BaseComponent, EverydayTypesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
